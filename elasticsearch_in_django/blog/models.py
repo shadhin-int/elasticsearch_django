fe558e0379a84d9f90c1f403a0df0991cf0c22c4
shadhin <shahinur.rahman@evaly.com.bd>
2021-05-24 16:31:42 +0600

    post model added

--- elasticsearch_in_django/blog/models.py
+++ elasticsearch_in_django/blog/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.utils.text import slugify
 
-# Create your models here.
+
+class Post(models.Model):
+	title = models.CharField(max_length=255, null=True, blank=True)
+	description = models.TextField(null=True, blank=True)
+	image = models.ImageField(upload_to='post_image')
+	body = models.TextField(blank=True, null=True)
+	order = models.IntegerField(blank=True, null=True)
+	slug = models.SlugField(default='', blank=True)
+
+	def save(self):
+		self.slug = slugify(self.title)
+		super(Post, self).save()
+
+	def __str__(self):
+		return self.title
